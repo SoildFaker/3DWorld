a6a2997c4853c4d76c497b9db26c626496701e73
Enbin <Enbin@DESKTOP-SF44B5T>
2016-05-07 18:12:16 +0800

    Add project files.

---
+++ 3DWorld/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _3DWorld.cls;
+
+namespace _3DWorld
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            double[,] = new double { [1, 2], [1, 2]}; 
+           Matrix a() ;
+        }
+    }
+}
---
+++ 3DWorld/cls/Matrix.cs
@@ -0,0 +1,12 @@
+﻿namespace _3DWorld.cls
+{
+    public sealed class Matrix
+    {
+        public Matrix() { }
+        public Matrix(double[,] member)
+        {
+
+        }
+
+    }
+}
